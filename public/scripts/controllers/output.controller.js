cdbc4e3916e7a0325ccf2d4f42c0999064a0dfe2
Andy Wolff <andy.wolff@gmail.com>
2016-12-01 13:31:07 -0600

    push

--- public/scripts/controllers/output.controller.js
+++ public/scripts/controllers/output.controller.js
@@ -1,25 +1,28 @@
-// salaryApp.controller('OutputController', ["$http", function ($http) {
-// 	console.log('employee input controller running');
-// 	var self = this;
-// 	self.newEmp = {};
-// 	self.employees = [];
-// 	getEmps();
-
-
 // expenditure output for table
-// salaryApp.controller('emp_outputController', ["$http", function ($http) {
-// 	console.log('expenditure output controller running');
-// 	var self = this;
-// 	calcSalary();
-//
-// 	self.calcSalary = function
-//
-//
-// }]);
+salaryApp.controller('OutputController', ["$http", function ($http) {
+	// console.log('expenditure output controller running');
+	var self = this;
+	getEmps();
+	self.employees = [];
+  self.totalSalary = 0.00;
 
+  function getEmps() {
+		$http.get('/salary_output')
+			.then(function (response) {
+				self.employees = response.data;
+        console.log('Employee Data Get ' + self.employees);
+        calcSalary();
+			});
+	}
 
-
-
+function calcSalary() {
+    self.totalSalary = 0.00;
+    self.employees.forEach(function(employee) {
+        self.totalSalary += Math.round(employee.yearly_salary / 12);
+        return;
+      });
+  }
+}]);
 //
 //
 //
@@ -95,4 +98,3 @@
 // 	total = total;
 // 	return total;
 // }
-// });
